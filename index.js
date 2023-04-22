97f628f80f0fce63652458c00a985d60993fa27a
sergiomarques <sergio@unicamp.br>
2023-04-21 23:09:00 -0300

    segundo teste

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
 const express = require('express')
-const webserver = express()
- .use((req, res) =>
+
+var webserver = express()
+
+webserver.use((req, res) =>
    res.sendFile('/websocket-client.html', { root: __dirname })
  )
- .listen(80, () => console.log(`Listening on ${80}`))
-const { WebSocketServer } = require('ws')
-const sockserver = new WebSocketServer({ port: 443 })
-sockserver.on('connection', ws => {
+
+
+ //.listen(80, () => console.log(`Listening on ${80}`))
+
+
+
+ var port = normalizePort(process.env.PORT || '3000');
+ webserver.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(webserver);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+
+
+ var port = normalizePort(process.env.PORT || '3000');
+ app.set('port', port);
+ 
+ function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+
+
+
+
+ const { WebSocketServer } = require('ws')
+
+ const sockserver = new WebSocketServer({ port: 443 })
+
+ sockserver.on('connection', ws => {
  console.log('New client connected!')
  ws.send('connection established')
  ws.on('close', () => console.log('Client has disconnected!'))
@@ -16,7 +64,9 @@ sockserver.on('connection', ws => {
      client.send(`${data}`)
    })
  })
+
  ws.onerror = function () {
    console.log('websocket error')
  }
-})
+}
+)
